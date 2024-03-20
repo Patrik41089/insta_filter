b1b6ee187f4dbc2972db8c452886bbdfa35e29a7
Patrik Radvák <rad41089@spseol.cz>
2024-03-20 12:09:28 +0100

    Posledni uprava

--- code_1.py
+++ code_1.py
@@ -1,17 +1,38 @@
 from PIL import Image
+from PIL import ImageFilter
 
-obrazek = Image.open("hunter.jpg")
-sirka, vyska = obrazek.size
+def apply_filter(image, filter_type):
+    width, height = image.size
 
-for x in range(sirka):
-    for y in range(vyska):
-        r, g, b = obrazek.getpixel((x,y))
-        
-        g = min(255, max(0, g + 10))
-        r = min(255, max(0, r + 80))
-        b = min(255, max(0, b + 40))
+    for x in range(width):
+        for y in range(height):
+            r, g, b = image.getpixel((x, y))
 
-        obrazek.putpixel((x,y), (r, g, b))
+            if filter_type == "Filter 1":
+                g = min(255, max(0, g + 50))
+            elif filter_type == "Filter 2":
+                r = min(255, max(0, r + 50))
+            elif filter_type == "Filter 3":
+                b = min(255, max(0, b + 50))
+            elif filter_type == "Filter 4":
+                image = image.filter(ImageFilter.BLUR)
 
-obrazek.show()
+            image.putpixel((x, y), (r, g, b))
+
+    return image
+
+def main():
+    image = Image.open("hunter.jpg")
+    filter_type = input("Vyberte filtr (1, 2, 3, 4): ")
+
+    if filter_type in ["1", "2", "3", "4"]:
+        image_filtered = apply_filter(image, "Filter " + filter_type)
+        image_filtered.show()
+    else:
+        print("Neplatný vstup.")
+
+if __name__ == "__main__":
+    main()
+
+
 
