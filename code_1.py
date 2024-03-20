9022850e3fd6a4c479d7a174161ab5299a1494be
Patrik Radvák <rad41089@spseol.cz>
2024-03-20 11:40:33 +0100

    nahrazení duplicitniho obrazku

--- code_1.py
+++ code_1.py
@@ -7,8 +7,8 @@ for x in range(sirka):
     for y in range(vyska):
         r, g, b = obrazek.getpixel((x,y))
         
-        g = min(255, max(0, g + 40))
-        r = min(255, max(0, r + 40))
+        g = min(255, max(0, g + 10))
+        r = min(255, max(0, r + 80))
         b = min(255, max(0, b + 40))
 
         obrazek.putpixel((x,y), (r, g, b))
